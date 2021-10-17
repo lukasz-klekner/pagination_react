9d5e212242cba912cb23cbef3416395f6c73eca2
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-17 12:56:04 +0200

    implements Pagination component and uses it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 import './App.css'
 import PostList from './components/PostList'
+import Pagination from './components/Pagination'
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
 
@@ -10,7 +11,7 @@ const App = () => {
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
-  const [postsPerPage, setPostsPerPage] = useState(10)
+  const [postsPerPage] = useState(10)
 
   useEffect(async () => {
     setLoading(true)
@@ -19,10 +20,21 @@ const App = () => {
     setLoading(false)
   }, [])
 
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className='container mt-5'>
       <h1 className='text-primary mb-3'>My blog</h1>
-      <PostList posts={posts} loading={loading} />
+      <PostList posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   )
 }
